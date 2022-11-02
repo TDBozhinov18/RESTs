515b4eab73d83f4f844c6fa10cd93c288dc4c959
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2022-11-02 14:53:47 +0200

    test

---
+++ src/main/java/com/example/rests/ResTsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rests/ResTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
